bbce8162669973fbe046ba66c70cd010aeef8dab
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-15 12:28:28 -0600

    se creo el paquete calculadora cientifica y el paquete calculadora estandar, tambien se agrego la clase calculadora estandar

---
+++ src/CalculadoraEstandar/CalculadoraEstandar.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalculadoraEstandar;
+
+/**
+ *
+ * @author adiseño.2015
+ */
+public class CalculadoraEstandar {
+    private int operando2;
+    
+    public void SetOperando(int op){
+        this.operando2=op;
+    }
+    public int getOperando(){
+        return this.operando2;
+    }
+    
+    
+}
